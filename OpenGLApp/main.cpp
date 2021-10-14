9c06a4036dbe839374787a5d0573aea0a22829fc
nevartS <stravenn@gmail.com>
2021-10-14 11:46:17 +0300

    Update on the Main.cpp
    Created Uniform variables for moving our triangle with offset and speed - [Straven]

--- OpenGLApp/main.cpp
+++ OpenGLApp/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <cmath>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -7,17 +8,25 @@
 // Window dimensions
 const GLint WIDTH = 1024, HEIGHT = 768;
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, shader, uniformXMove;
+
+// Variables
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.4f;  // Offset from the edges
+float triIncrement = 0.005f;  // Triangle positioning speed
 
 // Vertex Shader
-static const char* vShader = "									\n\
-#version 330													\n\
-																\n\
-layout (location = 0) in vec3 pos;								\n\
-																\n\
-void main()														\n\
-{																\n\
-	gl_Position = vec4(0.6 * pos.x, 0.6 * pos.y, pos.z, 1.0);	\n\
+static const char* vShader = "											\n\
+#version 330															\n\
+																		\n\
+layout (location = 0) in vec3 pos;										\n\
+																		\n\
+uniform float xMove;													\n\
+																		\n\
+void main()																\n\
+{																		\n\
+	gl_Position = vec4(0.6 * pos.x + xMove, 0.6 * pos.y, pos.z, 1.0);	\n\
 }";				
 
 // Fragment Shader
@@ -119,6 +128,8 @@ void CompileShaders()
 		printf("Error validating program: '%s'\n", eLog);
 		return;
 	}
+
+	uniformXMove = glGetUniformLocation(shader, "xMove");
 }
 
 int main()
@@ -184,11 +195,27 @@ int main()
 		// Get and Handle user input events
 		glfwPollEvents();
 
+		if (direction)
+		{
+			triOffset += triIncrement;
+		}
+		else
+		{
+			triOffset -= triIncrement;
+		}
+
+		if (abs(triOffset) >= triMaxOffset)
+		{
+			direction = !direction;
+		}
+
 		// Clear the window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
+
+		glUniform1f(uniformXMove, triOffset);
 		
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
